69c55fd9a5bc47f0f7a788999d8234e3460d5729
Lộc Nguyễn Xuân <locnxpd10746@gmail.com>
2024-06-14 11:43:25 +0700

    final

---
+++ Assignment/DAL_QLBanHang/DBConnect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL_QLBanHang
+{
+    public class DBConnect
+    {
+        protected SqlConnection _conn = new SqlConnection(@"Data Source=DESKTOP-H640B6J;Initial Catalog=QLBanHang;Integrated Security=True;Encrypt=False    ");
+    }
+}
